e79e279860b5241530f0e71f33615eee6088422e
eman <emankamalnasr96@gmail.com>
2021-03-27 05:38:23 +0200

    apply espresso details fragments

--- app/src/androidTest/java/com/example/nytimestask/ui/details/view/DetailsFragmentTest.kt
+++ app/src/androidTest/java/com/example/nytimestask/ui/details/view/DetailsFragmentTest.kt
@@ -2,7 +2,12 @@ package com.example.nytimestask.ui.details.view
 
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.nytimestask.R
 import com.example.nytimestask.data.model.Article
 import com.example.nytimestask.data.repository.FakeAndroidTestRepository
 import com.example.nytimestask.data.repository.IArticlesRepository
@@ -24,7 +29,9 @@ class DetailsFragmentTest {
         articlesFromApi = listOf<Article>(
             Article(id = 1, title = "article1"),
             Article(id = 2, title = "article2"),
-            Article(id = 3, title = "article3"),
+            Article(id = 3, title = "article3", source = "nytimes",
+                    byline = "eman", publishedDate = "27/3/2021",section = "Business",
+                    adxKeywords = "key1,key2",type = "article"),
             Article(id = 4, title = "article4"),
         )
         repository = FakeAndroidTestRepository(articlesFromApi)
@@ -41,7 +48,14 @@ class DetailsFragmentTest {
         val bundle = Bundle()
         bundle.putLong("articleID", article.data!!.id)
         launchFragmentInContainer<DetailsFragment>(bundle)
-
+        // then : article details are displayed on the screen
+        onView(withId(R.id.articleDetailsTitleTv)).check(matches(withText("article3")))
+        onView(withId(R.id.articleDetailsSourceTv)).check(matches(withText("nytimes")))
+        onView(withId(R.id.articleDetailsByLineTv)).check(matches(withText("eman")))
+        onView(withId(R.id.articleDetailsDateTv)).check(matches(withText("27/3/2021")))
+        onView(withId(R.id.articleDetailsSectionTv)).check(matches(withText("Business")))
+        onView(withId(R.id.articleDetailsKeywordsTv)).check(matches(withText("key1,key2")))
+        onView(withId(R.id.articleDetailsTypeTv)).check(matches(withText("Article")))
     }
 
     @After
